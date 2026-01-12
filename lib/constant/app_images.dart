cb3b92018fe939937fbf0d821f8eff6b46e9b5c4
sajad yoosuf <sajadyoosuf24@gmail.com>
2026-01-12 20:02:21 +0530

    files fully added

---
+++ lib/constant/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages{
+  static String app_background_image="assets/app_background_image.png";
+  static String white_circular="assets/white_circular.png";
+  static String background_shadow="assets/shadow.png";
+  static String circular_rounded="assets/circular_rounded.png";
+  static String app_logo="assets/logo.png";
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:space_app/constant/app_images.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AppImages.app_background_image),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Center(child: Image.asset(AppImages.background_shadow)),
+            Center(child: Image.asset(AppImages.circular_rounded)),
+            Center(child: Image.asset(AppImages.app_logo)),
+
+            Center(child: Image.asset(AppImages.white_circular)),
+          ],
+        ),
+      ),
+    );
+  }
+}
