8db99f5fb479686a4ef656dd256cc40ddd8443cc
Akhila <akhilapg637@gmail.com>
2026-01-17 11:04:24 +0530

    login page edited

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:space_app/constant/app_images.dart';
 
@@ -8,120 +10,135 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    // return Container(
-    //   width: width,
-    //   height: height,
-    //   decoration: BoxDecoration(
-    //     image: DecorationImage(image: AssetImage(AppImages.app_background_image),
-    //       fit: BoxFit.cover,
-    //     ),
-    //   ),
-    //   child: Scaffold(
-    //     backgroundColor: Colors.transparent,
-    //     body: Stack(
-    //       children: [Image.asset(AppImages.login_image)
-    //       ],
-    //     )
-    //     // Column(
-    //     //   children: [
-    //     //     Text("Sign in",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15,color: Colors.white),),
-    //     //   ],
-    //     // )
-    //   ),
-    // );
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            width: width,
-            height: height,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(AppImages.app_background_image),
-                fit: BoxFit.cover,
+
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AppImages.app_background_image),
+          fit: BoxFit.cover,
+        ),
+      ),
+
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [
+            SizedBox(height: 30),
+            Image.asset(AppImages.login_image, height: 50),
+            SizedBox(height: height * 0.16),
+            BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 0.0, sigmaY: 0.0),
+              child: Container(
+                width: double.infinity,
+                height: height * 0.7,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.12),
+                  border: Border.all(color: Colors.white.withOpacity(0.25)),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5),
+
+                      inputField(hint: "Email", icon: Icons.email),
+                      SizedBox(height: 5),
+                      inputField(
+                        hint: "Password",
+                        icon: Icons.lock,
+                        isPassword: true,
+                      ),
+                      SizedBox(height: 5),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Forgot Password?",
+                            style: TextStyle(color: Colors.cyanAccent),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Container(
+                        width: double.infinity,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.cyan,
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Sign in",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+
+                      Row(
+                        children: [
+                          Expanded(child: Divider()),
+                          Text(
+                            " or sign in using ",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Expanded(child: Divider()),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          socialIcons(AppImages.twittter_img),
+                          socialIcons(AppImages.facebook_img),
+                          socialIcons(AppImages.google_img),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Don't have an account?",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              color: Colors.cyanAccent,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
-          Column(
-            children: [
-              Image.asset(AppImages.login_image, height: 80),
-              SizedBox(height: 30),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Sign In",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-
-              inputField(hint: "Email", icon: Icons.email),
-              SizedBox(height: 10),
-              inputField(hint: "Password", icon: Icons.lock, isPassword: true),
-              SizedBox(height: 10),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Forgot Password?",
-                    style: TextStyle(color: Colors.cyanAccent),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                width: double.infinity,
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.cyan,
-                ),
-                child: Center(
-                  child: Text(
-                    "Sign in",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              Text("or sign in using", style: TextStyle(color: Colors.white)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  socialIcons(AppImages.twittter_img),
-                  socialIcons(AppImages.facebook_img),
-                  socialIcons(AppImages.google_img),
-                ],
-              ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account?",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Text(
-                    "Sign Up",
-                    style: TextStyle(
-                      color: Colors.cyanAccent,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
