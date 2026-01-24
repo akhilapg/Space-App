165c0b44ceffcbaaa2bb05e960c0ce0c7ac7999b
Akhila <akhilapg637@gmail.com>
2026-01-24 15:44:22 +0530

    favourite page details added

--- lib/screens/appbar_screen.dart
+++ lib/screens/appbar_screen.dart
@@ -1,20 +1,67 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:space_app/screens/favorites_screen.dart';
-// import 'package:space_app/screens/home_screen.dart';
-//
-// class AppbarScreen extends StatefulWidget {
-//   const AppbarScreen({super.key});
-//
-//   @override
-//   State<AppbarScreen> createState() => _AppbarScreenState();
-// }
-//
-// class _AppbarScreenState extends State<AppbarScreen> {
-//   List<Widget> screens =[HomeScreen(),FavoritesScreen()];
-//   @override
-//   Widget build(BuildContext context)
-//     return Container(
-//       ,
-//       );
-//   }
-// }
+import 'package:flutter/material.dart';
+
+PreferredSize buildAppbar(){
+  return PreferredSize(
+    preferredSize: Size.fromHeight(70),
+    child: AppBar(
+      flexibleSpace: Container(
+        // color: Colors.black.withOpacity(0.5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
+          border: Border(
+            bottom: BorderSide(color: Colors.black, width: 1.0),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              // margin: EdgeInsets.only(left: 40, top: 30),
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                color: Colors.transparent,
+                border: Border.all(color: Colors.black26, width: 2.0),
+              ),
+              child: Icon(Icons.density_medium, color: Colors.white),
+            ),
+            Column(
+              children: [
+                Text(
+                  "Milky Way",
+                  style: TextStyle(color: Colors.white, fontSize: 10),
+                ),
+                Text(
+                  "Solar System",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              // margin: EdgeInsets.only(left: 20, top: 30),
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                color: Colors.transparent,
+                border: Border.all(color: Colors.black26, width: 2.0),
+              ),
+              child: Icon(Icons.account_circle, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+
+      automaticallyImplyLeading: false,
+      backgroundColor: Colors.transparent,
+    ),
+  );
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:space_app/constant/solar_planet_details.dart';
+import 'package:space_app/screens/appbar_screen.dart';
 
 import '../constant/app_images.dart';
 
@@ -19,69 +20,7 @@ class HomeScreen extends StatelessWidget {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(70),
-          child: AppBar(
-            flexibleSpace: Container(
-              // color: Colors.black.withOpacity(0.5),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
-                ),
-                border: Border(
-                  bottom: BorderSide(color: Colors.black, width: 1.0),
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    // margin: EdgeInsets.only(left: 40, top: 30),
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      color: Colors.transparent,
-                      border: Border.all(color: Colors.black26, width: 2.0),
-                    ),
-                    child: Icon(Icons.density_medium, color: Colors.white),
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        "Milky Way",
-                        style: TextStyle(color: Colors.white, fontSize: 10),
-                      ),
-                      Text(
-                        "Solar System",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    // margin: EdgeInsets.only(left: 20, top: 30),
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      color: Colors.transparent,
-                      border: Border.all(color: Colors.black26, width: 2.0),
-                    ),
-                    child: Icon(Icons.account_circle, color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-
-            automaticallyImplyLeading: false,
-            backgroundColor: Colors.transparent,
-          ),
-        ),
+        appBar: buildAppbar(),
         body: Column(
           children: [
             SizedBox(height: 25),
